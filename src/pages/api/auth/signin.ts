d4fdc3a9fa6a87f99bd5e224e55d20728c202a34
cardullo <66775003+cardullo@users.noreply.github.com>
2024-01-26 17:45:31 +0100

    FreshDashboard draft

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -50,5 +50,5 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     secure: true,
   });
 
-  return redirect("/app/dashboard");
+  return redirect("/app/freshdashboard");
 };
